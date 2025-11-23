977865619b047deb20c38fca0704568388834857
OlesyaChuma <:lesechka1987@mail.ru>
2025-11-23 15:48:21 +0600

    Added cleanup of temporary merged and chunk files. Added formatted time output.

--- SortBigFile/SortBigFile.cpp
+++ SortBigFile/SortBigFile.cpp
@@ -4,27 +4,81 @@
 #include <iostream>
 #include <locale>
 #include <chrono>
+#include <windows.h>
 
 #include "Utils.h"
 #include "FileGenerator.h"
 #include "ChunkSorter.h"
 #include "FileMerger.h"
 
-void cleanupTempFiles(const std::vector<std::string>& files, const std::string& result) {
+// Удаление всех CHUNK-файлов (chunk_*.txt)
+void cleanupChunkFiles(const std::vector<std::string>& files,
+    const std::string& finalFile)
+{
     for (const auto& f : files) {
-        if (f != result) {
+        if (f != finalFile) {
             deleteFile(f);
         }
     }
 }
 
+// Удаление всех MERGED-файлов (merged_*.txt), кроме итогового
+void cleanupMergedFiles(const std::string& finalFile)
+{
+    // структура, в которую Windows запишет информацию о найденном файле
+    // Буква A означает ANSI-версию (FindFirstFileA)
+    WIN32_FIND_DATAA data;
+
+    // Эта функция ищет первый файл в папке, который подходит под маску
+    // и заполняет структуру data информацией о первом найденном файле
+    HANDLE h = FindFirstFileA("merged_*.txt", &data);
+
+    // Если найден хотя бы один файл — возвращает дескриптор поиска (HANDLE).
+    // Если таких файлов нет — возвращает INVALID_HANDLE_VALUE.
+    
+    if (h == INVALID_HANDLE_VALUE)
+        return;
+
+    do {
+        // Обрабатываем файл в data.cFileName
+        // цикл перебирает все файлы, подходящие под маску
+        std::string name = data.cFileName;
+
+        if (name != finalFile) {
+            deleteFile(name);
+        }
+
+    } while (FindNextFileA(h, &data));
+
+    FindClose(h);
+}
+
+// Функция форматирования времени(HH:MM:SS)
+std::string formatDuration(std::chrono::steady_clock::duration d) {
+    using namespace std::chrono;
+
+    auto secs = duration_cast<seconds>(d).count();
+
+    int hours = secs / 3600;
+    secs %= 3600;
+
+    int minutes = secs / 60;
+    secs %= 60;
+
+    int seconds = secs;
+
+    char buffer[32];
+    std::snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d", hours, minutes, seconds);
+    return std::string(buffer);
+}
+
 int main() {
     setlocale(LC_ALL, "");
 
-    auto mark = [](std::string name, auto start, auto end) {
+    auto mark = [](const std::string& name, auto start, auto end) {
         std::cout << name << ": "
-            << std::chrono::duration_cast<std::chrono::seconds>(end - start).count()
-            << " сек\n";
+            << formatDuration(end - start)  
+            << "\n";
         };
 
     auto t0 = std::chrono::steady_clock::now();
@@ -51,7 +105,10 @@ int main() {
     mark("Слияние файлов", t4, t5);
 
     // 4. Удаление временных файлов (все chunk_*.txt)
-    cleanupTempFiles(chunks, finalFile);
+    cleanupChunkFiles(chunks, finalFile);
+
+    // 5. Удаляем merged-файлы
+    cleanupMergedFiles(finalFile);
 
     std::cout << "\nГОТОВО! Итоговый отсортированный файл: "
         << finalFile << "\n";
